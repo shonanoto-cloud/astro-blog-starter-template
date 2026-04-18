050514217146ac3ffdfafec2a15a5805aabe8c0b
shonan_oto <shonanoto@gmail.com>
2026-04-18 22:41:38 +0900

    image test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import image from '@astrojs/image';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), image()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
